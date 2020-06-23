b0677dadc81eefa8e44a3448a9c1a46e93628ebb
YTHU <17494581@qq.com>
2020-06-23 16:57:48 +0800

    null

--- SpringCloud/Service/service/src/main/java/ythu/everything/service/controller/ServiceController.java
+++ SpringCloud/Service/service/src/main/java/ythu/everything/service/controller/ServiceController.java
@@ -1,10 +1,9 @@
 package ythu.everything.service.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/xhr")
@@ -13,6 +12,13 @@ public class ServiceController {
     @ResponseBody
     @GetMapping("/get")
     public String get() {
-        return "Hello";
+        return "Hello Vue";
+    }
+
+    @ResponseBody
+    @PostMapping("/post")
+    public String post(HttpServletResponse response) {
+        response.setStatus(500);
+        return "Hello Vue";
     }
 }
